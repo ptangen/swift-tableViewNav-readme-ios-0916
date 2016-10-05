bf60be2af48c60cc53d7f81a23a5facc25189d8f
Paul Tangen <ptangen@ptangen.com>
2016-10-05 10:02:39 -0400

    Complete the tableview Nav readme.

--- NavStuff/TableViewController.swift
+++ NavStuff/TableViewController.swift
@@ -15,11 +15,41 @@ class TableViewController: UITableViewController {
         return 1
     }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
+   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dirtyDancingSoundtrack.count
+   }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DirtyCell", for: indexPath)
+        let songTitle = dirtyDancingSoundtrack[indexPath.row]
+        cell.textLabel?.text = songTitle
+        return cell
     }
+    
+    let dirtyDancingSoundtrack = [
+        "(I've Had) The Time of My Life",
+        "Be My Baby",
+        "She's Like the Wind",
+        "Hungry Eyes",
+        "STAY",
+        "Yes",
+        "You Don't Own Me",
+        "Hey Baby",
+        "Overload",
+        "Love Is Strange",
+        "Where Are You Tonight?",
+        "In the Still of the Night",
+        "(I've Had) The Time of My Life",
+        "Be My Baby",
+        "She's Like the Wind",
+        "Hungry Eyes",
+        "STAY",
+        "Yes",
+        "You Don't Own Me",
+        "Hey Baby",
+        "Overload",
+        "Love Is Strange",
+        "Where Are You Tonight?",
+        "In the Still of the Night"
+    ]
 }
